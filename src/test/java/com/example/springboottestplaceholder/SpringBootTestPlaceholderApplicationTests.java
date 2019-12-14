5503174b3bbeaf1a79b41dc3b8abb233ceb3e6ab
Cnfn <cnfnss@gmail.com>
2019-12-14 20:27:11 +0800

    first commmit

---
+++ src/test/java/com/example/springboottestplaceholder/SpringBootTestPlaceholderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottestplaceholder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestPlaceholderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
